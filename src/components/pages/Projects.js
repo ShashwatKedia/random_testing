15a81b0123c880c7dc952a845dcb65bf9260ed23
ShashwatKedia <be23b022@smail.iitm.ac.in>
2023-12-08 11:55:02 +0530

    Projects main page template completed

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -26,43 +26,44 @@ const Projects = () => {
           {/* Card 2 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image2.jpg' alt='Card Image 2' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 2</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 3 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image3.jpg' alt='Card Image 3' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 3</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 4 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='../../../public/images/img13.png' alt='Card Image 4' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 4</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
+
         </div>
         <GrTopCorner className='tp_corner' />
-        <h1 className='projects_heading'>YEAR LONG PROJECTS</h1>
+        <h1 className='projects_heading'>ANNUAL PROJECTS</h1>
         <GrBottomCorner className='bm_corner' />
         <div className='cards-row'>
           {/* Render the four cards for YEAR LONG PROJECTS */}
           {/* Card 1 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='img1.png' alt='Card Image 1' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
               <h3>Card 1</h3>
@@ -72,33 +73,34 @@ const Projects = () => {
           {/* Card 2 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image2.jpg' alt='Card Image 2' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 2</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 3 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image3.jpg' alt='Card Image 3' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 3</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 4 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image4.jpg' alt='Card Image 4' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 4</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
+
         </div>
         <GrTopCorner className='tp_corner' />
         <h1 className='projects_heading'>YEAR LONG PROJECTS</h1>
@@ -108,7 +110,7 @@ const Projects = () => {
           {/* Card 1 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='img1.png' alt='Card Image 1' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
               <h3>Card 1</h3>
@@ -118,43 +120,44 @@ const Projects = () => {
           {/* Card 2 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image2.jpg' alt='Card Image 2' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 2</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 3 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image3.jpg' alt='Card Image 3' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 3</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 4 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image4.jpg' alt='Card Image 4' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 4</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
+
         </div>
         <GrTopCorner className='tp_corner' />
-        <h1 className='projects_heading'>YEAR LONG PROJECTS</h1>
+        <h1 className='projects_heading'>ANNUAL PROJECTS</h1>
         <GrBottomCorner className='bm_corner' />
         <div className='cards-row'>
           {/* Render the four cards for YEAR LONG PROJECTS */}
           {/* Card 1 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='img1.png' alt='Card Image 1' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
               <h3>Card 1</h3>
@@ -164,34 +167,36 @@ const Projects = () => {
           {/* Card 2 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image2.jpg' alt='Card Image 2' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 2</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 3 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image3.jpg' alt='Card Image 3' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 3</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
           {/* Card 4 */}
           <div className='card'>
             <div className='card-image'>
-              <img src='placeholder-image4.jpg' alt='Card Image 4' />
+              <img src={require('./img1.jpg')} alt='Card Image 1' />
             </div>
             <div className='card-text'>
-              <h3>Card 4</h3>
+              <h3>Card 1</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </div>
           </div>
+
         </div>
+
       </div>
 
       {/* Rest of the sections follow the same structure as above */}
